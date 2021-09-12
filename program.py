ae86668587ec1f694b2c91c7c2cb6ad29240e6ed
skzxx <rrachmawan1965@gmail.com>
2021-09-12 22:53:31 +0700

    add

---
+++ program.py
@@ -0,0 +1,15 @@
+#Data type in python (01) - integer
+#I'm use type as method
+Operator_Data_Type_Integer = 10;
+print("Data type integer: ", Operator_Data_Type_Integer, type(Operator_Data_Type_Integer), "\n");
+#Float data type (2)
+Operator_Data_Type_Float = 3.1415;
+print("Data type float: ", Operator_Data_Type_Float, type(Operator_Data_Type_Float), "\n");
+#string daya type
+Operator_Data_Type_String = "This is a letter";
+print("Data type string: ", Operator_Data_Type_String, type(Operator_Data_Type_String), "\n");
+#boolean data type
+Operator_Data_Type_Boolean = True;
+print("Data type boolean: ", Operator_Data_Type_Boolean, type(Operator_Data_Type_Boolean), "\n");
+
+
